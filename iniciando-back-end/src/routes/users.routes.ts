3539df3320a8aaa676e1f03a9b602b6dc2ec14c3
Lucas Mattos <lcsmattos7@gmail.com>
2020-11-11 17:39:33 -0300

    Inicio do Back-end

---
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+// Rota: Receber a requisição, chamar outro arquivo e devolver uma resposta
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default usersRouter;
