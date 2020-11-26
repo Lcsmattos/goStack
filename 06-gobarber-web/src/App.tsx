2c2ffdd40300508b1c428788ea73fae9b89ffef0
Lucas Mattos <lcsmattos7@gmail.com>
2020-11-26 15:05:58 -0300

    Criaçao pagina de cadastro/Isolando componentes

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello Lucas</h1>;
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
 
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
 export default App;
